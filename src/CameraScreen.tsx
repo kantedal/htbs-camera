ccde1005ee6070e15ff9af34afe4c2479ecd5431
Emil Juopperi <emiju987@student.liu.se>
2018-09-01 00:31:28 +0200

    flip2

--- src/CameraScreen.tsx
+++ src/CameraScreen.tsx
@@ -1,18 +1,26 @@
-import { Camera, Permissions } from 'expo'
-import { GLSL, Node, Shaders } from 'gl-react'
-import * as GLView from 'gl-react-expo'
-import * as React from 'react'
-import { Component } from 'react'
-import { Image, Text, TouchableOpacity, View } from 'react-native'
+import { Camera, Permissions } from "expo";
+import { GLSL, Node, Shaders } from "gl-react";
+import * as GLView from "gl-react-expo";
+import * as React from "react";
+import { Component } from "react";
+import {
+  Animated,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+  Easing
+} from "react-native";
 
-const Camera2: any = Camera
+const Camera2: any = Camera;
 
-const gui = require('./gui.png')
+const gui = require("./gui.png");
 
 interface State {
-  cameraType: string // TODO Camera.Constants.Type,
-  hasPermissionToCamera: boolean | undefined
-  path: string
+  cameraType: string; // TODO Camera.Constants.Type,
+  hasPermissionToCamera: boolean | undefined;
+  path: string;
+  spinDegree: any;
 }
 
 const shaders = Shaders.create({
@@ -33,102 +41,139 @@ const shaders = Shaders.create({
       }
     `
   }
-})
+});
 
 export class CameraScreen extends Component<{}, State> {
-  private _timer: any
-  private _camera: any
+  private _timer: any;
+  private _camera: any;
 
   constructor(props: {}, context?: any) {
-    super(props, context)
+    super(props, context);
 
     this.state = {
-      cameraType: 'back',
+      cameraType: "back",
       hasPermissionToCamera: undefined,
-      path: '',
-    }
+      path: "",
+      spinDegree: new Animated.Value(0)
+    };
   }
 
-  private onLayout = (event) => this.start()
+  private flipUi = () => {
+    return Animated.timing(
+      // Animate over time
+      this.state.spinDegree, // The animated value to drive
+      {
+        toValue: 1, // Animate to opacity: 1 (opaque)
+        duration: 1000, // Make it take a while
+        easing: Easing.back(1)
+      }
+    ).start(); // Starts the animation
+  };
+
+  private onLayout = event => this.start();
 
   private refreshPic = () => {
     this._camera
-      .capture({ jpegQuality: 70, target: Camera2.Constants.CaptureTarget.temp })
+      .capture({
+        jpegQuality: 70,
+        target: Camera2.Constants.CaptureTarget.temp
+      })
       .then(data => this.setState({ path: data.path }))
-      .catch(err => console.log(err))
-  }
+      .catch(err => console.log(err));
+  };
 
   private start() {
-    this._timer = setInterval(() => this.refreshPic(), 5)
+    this._timer = setInterval(() => this.refreshPic(), 5);
   }
 
   public async componentWillMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA)
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
 
     this.setState({
-      hasPermissionToCamera: status === 'granted'
-    })
+      hasPermissionToCamera: status === "granted"
+    });
   }
 
   public render() {
     if (this.state.hasPermissionToCamera === undefined) {
-      return <Text>Getting permission to access the camera.</Text>
+      return <Text>Getting permission to access the camera.</Text>;
     }
 
     if (this.state.hasPermissionToCamera === false) {
-      return <Text>No access to the camera.</Text>
+      return <Text>No access to the camera.</Text>;
     }
+    console.log(this.state.spinDegree);
 
     return (
-      <View style={{ flex: 1 }}>
+      <Animated.View
+        style={{
+          flex: 1,
+          transform: [
+            {
+              rotateX: this.state.spinDegree.interpolate({
+                inputRange: [0, 1],
+                outputRange: ["0deg", "360deg"]
+              })
+            }
+          ]
+        }}
+      >
         <Camera2
           style={{ flex: 1 }}
           type={this.state.cameraType}
-          ref={cam => this._camera = cam}
-        // captureQuality={Camera2.Constants.CaptureQuality['720p']}
-        // aspect={Camera2.Constants.Aspect.fill}
+          ref={cam => (this._camera = cam)}
+          // captureQuality={Camera2.Constants.CaptureQuality['720p']}
+          // aspect={Camera2.Constants.Aspect.fill}
         >
-          <View style={{ backgroundColor: 'transparent', flex: 1, flexDirection: 'row' }}>
+          <View
+            style={{
+              backgroundColor: "transparent",
+              flex: 1,
+              flexDirection: "row"
+            }}
+          >
             <Image
-              style={{ width: '100%', height: '100%', position: 'absolute' }}
-              source={{ uri: 'https://i.imgur.com/qTfJq6h.png' }}
+              style={{ width: "100%", height: "100%", position: "absolute" }}
+              source={{ uri: "https://i.imgur.com/qTfJq6h.png" }}
             />
             <TouchableOpacity
               onPress={() => this.toggleCameraType()}
               style={{
-                alignItems: 'center',
-                alignSelf: 'flex-end',
+                alignItems: "center",
+                alignSelf: "flex-end",
                 flex: 0.1
               }}
             >
               <Text
                 style={{
-                  color: 'white',
+                  color: "white",
                   fontSize: 18,
                   marginBottom: 10
                 }}
               >
-                {' '}Flip{' '}
+                Flip
               </Text>
             </TouchableOpacity>
           </View>
         </Camera2>
         {/* <GLView.Surface style={{ width: 100, height: 100 }}>
-          <Node shader={shaders.Saturate} uniforms={{ contrast: 1, saturation: 2, brightness: 1, t: { uri: 'https://i.imgur.com/uTP9Xfr.jpg' } }} />
-        </GLView.Surface> */}
-      </View>
-    )
+            <Node shader={shaders.Saturate} uniforms={{ contrast: 1, saturation: 2, brightness: 1, t: { uri: 'https://i.imgur.com/uTP9Xfr.jpg' } }} />
+          </GLView.Surface> */}
+      </Animated.View>
+    );
   }
 
   private toggleCameraType() {
-    this.setState({
-      // TODO
-      // cameraType: this.state.cameraType === Camera.Constants.Type.back
-      //   ? Camera.Constants.Type.front
-      //   : Camera.Constants.Type.back
-      cameraType: this.state.cameraType === 'back'
-        ? 'front'
-        : 'back'
-    })
+    this.flipUi();
+    setTimeout(() => {
+      this.setState({
+        // TODO
+        // cameraType: this.state.cameraType === Camera.Constants.Type.back
+        //   ? Camera.Constants.Type.front
+        //   : Camera.Constants.Type.back
+        spinDegree: new Animated.Value(0),
+        cameraType: this.state.cameraType === "back" ? "front" : "back"
+      });
+    }, 1000);
   }
 }
